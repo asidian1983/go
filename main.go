9823eda256268e235a2eb28d3e13fa3fed497569
asidian1983 <asidian@gmail.com>
2024-01-07 22:18:42 +0900

    for

--- main.go
+++ main.go
@@ -9,6 +9,15 @@ func multiply(a, b int) int {
 	return a * b
 }
 
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+
+	return total
+}
+
 func lenAndUpper(name string) (lenght int, uppercase string) {
 	defer fmt.Println("Done") // func 끝나고 실행 됨
 	lenght = len(name)
@@ -32,4 +41,7 @@ func main() {
 	fmt.Println(totalLengtht, upperText)
 
 	repeat("1", "2", "3")
+
+	result := superAdd(1, 2, 3, 4, 5)
+	fmt.Println(result)
 }
