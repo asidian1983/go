0a5bed2a4b57fa19429dc96486861ab45682db56
asidian1983 <asidian@gmail.com>
2024-01-07 22:01:40 +0900

    func, data types test

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
 
 func main() {
-	fmt.Println("Hello world")
+	const name string = "test"
+	test := false
+	var male bool = false
+
+	fmt.Println(test, male)
+	fmt.Println(multiply(2, 2))
+
+	totalLengtht, upperText := lenAndUpper(name)
+	fmt.Println(totalLengtht, upperText)
 }
